7e1956e59ec9a9ffb23bdf3ab6b60b55f3c56a6f
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-03-26 21:11:38 +0200

    first 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,110 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Button from './Button';
+import { nanoid } from 'nanoid';
+
+const reseter = {
+  name: '',
+  number: '',
 };
+
+class Phonebook extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+    search: '',
+  };
+
+  render() {
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.search.toLowerCase())
+    );
+
+    return (
+      <div className="all-pos">
+        <ul className="flexator">
+          <li>
+            <form action="">
+              <h1>Phonebook</h1>
+              <div className="form">
+                <h3 className="area-for-enter">Name</h3>
+                <input
+                  type="text"
+                  name="name"
+                  value={this.state.name}
+                  className="form-control"
+                  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                  required
+                  onChange={e => {
+                    this.setState({ name: e.target.value });
+                  }}
+                />
+                <h3 className="area-for-enter">Number</h3>
+                <input
+                  type="tel"
+                  name="number"
+                  value={this.state.number}
+                  className="form-control"
+                  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                  required
+                  onChange={e => {
+                    this.setState({ number: e.target.value });
+                  }}
+                />
+              </div>
+              <button
+                className="btn btn-primary add-contact"
+                onClick={e => {
+                  e.preventDefault();
+                  const newContact = {
+                    id: nanoid(),
+                    name: this.state.name,
+                    number: this.state.number,
+                  };
+                  this.setState({
+                    contacts: [...this.state.contacts, newContact],
+                    name: '',
+                    number: '',
+                  });
+                }}
+              >
+                Add contact
+              </button>
+            </form>
+          </li>
+          <li>
+            <h1>Contacts</h1>
+            <input
+              type="text"
+              name="search"
+              value={this.state.search}
+              className="form-control"
+              placeholder="Search by name"
+              onChange={e => {
+                this.setState({ search: e.target.value });
+              }}
+            />
+            <div className="scrollable-list">
+              <ul className="listContacts">
+                {filteredContacts.map(e => (
+                  <li key={e.id}>
+                    <b>{e.name}</b> - {e.number}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+    Phonebook.propTypes = {
+      contacts: PropTypes.object.isRequired,
+    };
+  }
+}
+
+export default Phonebook;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import Phonebook from 'components/App';
+import nanoid from 'nanoid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Phonebook />
   </React.StrictMode>
 );
