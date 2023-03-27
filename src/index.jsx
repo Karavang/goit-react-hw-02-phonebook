64da79ab3f0811b6e66c37138b13075fe2007429
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-03-27 11:06:20 +0200

    test's

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Phonebook from 'components/App';
-import nanoid from 'nanoid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
