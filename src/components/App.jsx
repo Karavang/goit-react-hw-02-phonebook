51f64b642dbace9abc1e0be3bf64c679cde51a39
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-03-29 14:46:16 +0200

    workable

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,8 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import React from 'react';
-// import Button from './Button';
 import { nanoid } from 'nanoid';
-
+import Form from './Form';
 class Phonebook extends Component {
   state = {
     contacts: [],
@@ -11,64 +10,32 @@ class Phonebook extends Component {
     number: '',
     search: '',
   };
-
+  addContact = (name, number) => {
+    const newContact = {
+           id: nanoid(),
+           name: name,
+           number: number,
+         };
+ 
+    this.setState(prevState => ({
+       contacts: [...prevState.contacts, newContact],
+     }));
+   };
   render() {
     const filteredContacts = this.state.contacts.filter(contact =>
       contact.name.toLowerCase().includes(this.state.search.toLowerCase())
     );
-
+   
+    
     return (
       <div className="all-pos">
         <ul className="flexator">
           <li>
             <form action="">
               <h1>Phonebook</h1>
-              <div className="form">
-                <h3 className="area-for-enter">Name</h3>
-                <input
-                  type="text"
-                  name="name"
-                  value={this.state.name}
-                  className="form-control"
-                  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                  required
-                  onChange={e => {
-                    this.setState({ name: e.target.value });
-                  }}
-                />
-                <h3 className="area-for-enter">Number</h3>
-                <input
-                  type="tel"
-                  name="number"
-                  value={this.state.number}
-                  className="form-control"
-                  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                  required
-                  onChange={e => {
-                    this.setState({ number: e.target.value });
-                  }}
-                />
-              </div>
-              <button
-                className="btn btn-primary add-contact"
-                onClick={e => {
-                  e.preventDefault();
-                  const newContact = {
-                    id: nanoid(),
-                    name: this.state.name,
-                    number: this.state.number,
-                  };
-                  this.setState({
-                    contacts: [...this.state.contacts, newContact],
-                    name: '',
-                    number: '',
-                  });
-                }}
-              >
-                Add contact
-              </button>
+           
+<Form addContact={this.addContact } />
+              
             </form>
           </li>
           <li>
@@ -101,4 +68,5 @@ class Phonebook extends Component {
 Phonebook.propTypes = {
   contacts: PropTypes.object.isRequired,
 };
+
 export default Phonebook;
